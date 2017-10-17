dff6235503aaf0def8680735347b56c72a57817b
mgxnzhu <mgxzhu@gmail.com>
2017-10-17 15:32:43 +0100

    output python script for test

---
+++ src/mapgenrator/test.cpp
@@ -0,0 +1,27 @@
+#include "noise.h"
+#include "vector_arithmetic.hpp"
+#include "vector_maxmin.hpp"
+#include "randommap.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+int main(){
+
+  auto map = setmap(24,24,1.7,1);
+  std::ofstream fout("show.py");
+
+  fout<<"import matplotlib.pyplot as plt\n";
+  fout<<"import numpy as np\n";
+  fout<<"randmap = np.array("<<map<<")\n";
+  fout<<"plt.figure()\n";
+  fout<<"plt.imshow(randmap,cmap=plt.cm.YlGn)\n";
+  fout<<"plt.show()\n";
+
+  fout<<std::flush;
+  fout.close();
+
+
+  return 0;
+}
+
