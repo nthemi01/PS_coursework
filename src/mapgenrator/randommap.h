fc6db93f77a8b8c23c329012e4fbd830bf84d5bf
mgxnzhu <mgxzhu@gmail.com>
2017-10-17 16:12:29 +0100

    generate random 2d map

--- src/mapgenrator/randommap.h
+++ src/mapgenrator/randommap.h
@@ -8,4 +8,11 @@
 
 std::vector<std::vector<double>> setmap(int x, int y, double index = 1.7, const int samples = 10, const int octave = 4);
 
+// input:
+//   x,y : the size of output map
+//   index : the index of the power function applied on values of the map to make the terrain steeper (index>1) or smoother (index<1)
+//   samples : number of points between two neighbor random points
+//   octave : order of the highest frequency noise
+// return a 2d random map (values from 0 to 1) in size of (x,y)
+
 #endif
