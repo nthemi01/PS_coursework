64c57d5afa31b8a2c32913221fd20a1779202eb8
nthemi01 <themistokleous.neofytos@gmail.com>
2017-10-18 23:55:27 +0100

    landscape class: progress & get functions done.still working on constructor

--- include/landscape.h
+++ include/landscape.h
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
+#include <algorithm>
 #include <valarray>
 
 #ifndef LANDSCAPE_H
 #define LANDSCAPE_H
 
+//Info for the rest of the programmers:
+//	Fow now the constructor doesn't accept arguments
+//	To go to the next time step use: progress()
+//	To get the values of hares and pumas use:
+//		get_hares();
+//		get_pumas();
+
 
 class landscape {
 private:
-	int grid_size_x, grid_size_y;
+	int grid_size_x, grid_size_y;  //spacial dimensions of the grid
 	double r, a, b, m, k, l ,dt; // where ... 
 	std::vector<std::vector<double> > pumas; //the density matrix of pumas
 	std::vector<std::vector<double> > hares; // the density matrix of hares
@@ -18,26 +26,30 @@ private:
 	std::vector<std::vector<bool> > grid; // 1 represents land, 0 represents water 
 	std::vector<std::vector<int> > N; // the number of "dry" neighboring squares
 
-	std::vector<std::vector<double> > pumas_old; //the density matrix of pumas
-	std::vector<std::vector<double> > hares_old; // the density matrix of hares
-				//also maybe something about the squares filled with water
+	std::vector<std::vector<double> > pumas_old; //temporary storing the  density matrix of pumas for use from the progress function
+	std::vector<std::vector<double> > hares_old;
+
 public:
 
-	landscape();
-	void progress();
-	double average_hares();
+	landscape(); //constractor set the right grid size, initiates densities, calculates number of neighbors
+	void progress(); // calculate the density values of hares and pumas for the next time step
+	double average_hares(); //return the average value of hares along the "dry" squares
 	double average_pumas();
-	void printPPM();
 
-	void printhares();
+	void printhares(); //prints to the screen the density matrix of hares
 	void printpumas();
 
 	void printx();//just a temporary test function
 
-	
+	//A neat way to resize vectors of vectors. As soon as I figure out how, I will use prototype to reduce these three to one function.
 	void resizeVec( std::vector<std::vector<bool> > &vec , const unsigned short ROWS , const unsigned short COLUMNS );
 	void resizeVec( std::vector<std::vector<int> > &vec , const unsigned short ROWS , const unsigned short COLUMNS );
 	void resizeVec( std::vector<std::vector<double> > &vec , const unsigned short ROWS , const unsigned short COLUMNS );
+
+	std::vector<std::vector<double> > get_hares();//returns a vector of vectors with size grid_size_x * grid_size_y with the density of hares
+	std::vector<std::vector<double> > get_pumas();
+
+
 };
 
 #endif
--- src/landscape.cpp
+++ src/landscape.cpp
@@ -6,13 +6,20 @@ landscape::landscape()
 	grid_size_x = 9;
 	grid_size_y=9;
 
-
-	resizeVec(grid,grid_size_x,grid_size_y);
-	resizeVec (N,grid_size_x,grid_size_y);
+	resizeVec(grid,grid_size_x,grid_size_y);	// resize and initialize grid with some default values for now	
 	for(int i=0;i!=grid_size_x;i++)
 		for(int j=0;j!=grid_size_y;j++)
-		{
-		}	
+			if( i==0 || i==grid_size_x-1 || j==0 || j==grid_size_y-1)
+				grid[i][j]=0;
+			else if( (i==4 && j==4) || (i==3 && j==4))
+				grid[i][j]=0;
+			else
+				grid[i][j]=1;
+
+	resizeVec (N,grid_size_x,grid_size_y); // N counts the number of neighbors for each cell in order to be used from the progress function
+	for(int i=1;i!=grid_size_x-1;i++)
+		for(int j=1;j!=grid_size_y-1;j++)
+			N[i][j]=grid[i+1][j]+grid[i][j+1]+grid[i-1][j]+grid[i][j-1];
 
 	resizeVec(pumas,grid_size_x,grid_size_y);
 	resizeVec(hares,grid_size_x,grid_size_y);
@@ -20,17 +27,22 @@ landscape::landscape()
 	resizeVec(pumas_old,grid_size_x,grid_size_y);
 	resizeVec(hares_old,grid_size_x,grid_size_y);
 
+	
+
 	for(int i=0;i!=grid_size_x;i++)
 		for(int j=0;j!=grid_size_y;j++)
 		{
-			if( i==0 || i==grid_size_x-1 || j==0 || j==grid_size_y-1)
+			if( grid[i][j]==1)
 			{
-				pumas[i][j]=0;
-				hares[i][j]=0;
-				continue;
+				if( i==0 || i==grid_size_x-1 || j==0 || j==grid_size_y-1)
+				{
+					pumas[i][j]=0;
+					hares[i][j]=0;
+					continue;
+				}
+				hares[i][j]= i*1./2;
+				pumas[i][j]= j*1./2;
 			}
-			hares[i][j]= i;
-			pumas[i][j]= j;
 		}
 			
 			
@@ -44,12 +56,19 @@ void landscape::progress()
 	pumas_old.swap(pumas);
 	hares_old.swap(hares);
 
-	for(int i=0;i!=grid_size_x;i++)
-		for(int j=0;j!=grid_size_y;j++)
-		{
-			double part1,part2,part3,part4;
+	for(int i=1;i!=grid_size_x-1;i++)
+		for(int j=1;j!=grid_size_y-1;j++)
+			if(grid[i][j])
+			{
+				double part1,part2;
 
-			part1= hares_old[i-1][j]+hares_old[i+1][j]+hares_old[i][j-1]+hares_old[i][j+1];	
+				part1 = hares_old[i-1][j]+hares_old[i+1][j]+hares_old[i][j-1]+hares_old[i][j+1];	
+				part2 = k*(part1 - N[i][j]*hares_old[i][j]);
+				hares[i][j] = hares_old[i][j] + dt*( r*hares_old[i][j] - a*hares_old[i][j]*pumas_old[i][j] + part2);
+			 
+				part1 = pumas_old[i-1][j]+pumas_old[i+1][j]+pumas_old[i][j-1]+pumas_old[i][j+1];	
+				part2 = l*(part1 - N[i][j]*pumas_old[i][j]);			
+				pumas[i][j] = pumas_old[i][j] + dt*( b*hares_old[i][j]*pumas_old[i][j] - m*pumas_old[i][j] + part2);
 		}	
 
 	// progress function will be the one doing the actual computation for both Pumas and Hares
@@ -74,12 +93,11 @@ void landscape::printPPM()
 
 void landscape::printx()
 {
-	int x=5;	
-	progress();
-	printhares();
-	printpumas();
-	//std::cout<<"X: "<<x<<std::endl;
-	//std::cout<<pumas.size()<<std::endl;
+	int x=5;
+	for(int i=0;i!=10;i++)	
+		progress();
+	//printhares();
+	//printpumas();
 }
 void landscape::resizeVec( std::vector<std::vector<double> > &vec , const unsigned short ROWS , const unsigned short COLUMNS )
 {
@@ -113,11 +131,11 @@ void landscape::resizeVec( std::vector<std::vector<int> > &vec , const unsigned 
 
 void landscape::printhares()
 {
-std::cout<<"Hares:\n\n";
+std::cout<<"hares:\n\n";
 
 for (int i = 0; i < grid_size_x; i++ ) {
       for (int j = 0; j < grid_size_y; j++ ) {
-         std::cout << hares_old[i][j] << " ";
+         std::cout << hares[i][j] << "\t\t\t";
       }
       std::cout << std::endl;
    }
@@ -126,12 +144,24 @@ for (int i = 0; i < grid_size_x; i++ ) {
 
 void landscape::printpumas()
 {
-std::cout<<"\nPumas:\n\n";
+std::cout<<"\npumas:\n\n";
 
 for (int i = 0; i < grid_size_x; i++ ) {
       for (int j = 0; j < grid_size_y; j++ ) {
-         std::cout << pumas_old[i][j] << " ";
+         std::cout << pumas[i][j] << "\t\t\t";
       }
       std::cout << std::endl;
    }
+}
+
+
+std::vector<std::vector<double> > landscape::get_hares()
+{
+return hares;
+}
+
+
+std::vector<std::vector<double> > landscape::get_pumas()
+{
+return pumas;
 }
