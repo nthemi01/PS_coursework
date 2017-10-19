2c8aaa3bdde3e8688e5d1106fad2f735cf4650cf
Conor Forgie <s1326353@sms.ed.ac.uk>
2017-10-19 19:18:57 +0100

    Updated the constructor for the class landscape.

--- include/landscape.h
+++ include/landscape.h
@@ -3,6 +3,10 @@
 #include <vector>
 #include <algorithm>
 #include <valarray>
+#include <fstream> //for reading in files
+#include <ctime> //for seeding random number generator
+#include <string.h>
+
 
 #ifndef LANDSCAPE_H
 #define LANDSCAPE_H
@@ -21,6 +25,7 @@ private:
 	double r, a, b, m, k, l ,dt; // where ... 
 	std::vector<std::vector<double> > pumas; //the density matrix of pumas
 	std::vector<std::vector<double> > hares; // the density matrix of hares
+	std::vector<std::vector<double> > map; // map matrix
 
 
 	std::vector<std::vector<bool> > grid; // 1 represents land, 0 represents water 
@@ -28,16 +33,22 @@ private:
 
 	std::vector<std::vector<double> > pumas_old; //temporary storing the  density matrix of pumas for use from the progress function
 	std::vector<std::vector<double> > hares_old;
+	
+	std::string pumaDpath; // path of density & map files
+	std::string hareDpath;
+	std::string mapPath;
+	
 
 public:
 
-	landscape(); //constractor set the right grid size, initiates densities, calculates number of neighbors
+	landscape(const struct Params &, const std::string , const std::string, const std::string); //constractor set the right grid size, initiates densities, calculates number of neighbors
 	void progress(); // calculate the density values of hares and pumas for the next time step
 	double average_hares(); //return the average value of hares along the "dry" squares
 	double average_pumas();
 
 	void printhares(); //prints to the screen the density matrix of hares
 	void printpumas();
+	void printPPM();
 
 	void printx();//just a temporary test function
 
@@ -49,7 +60,14 @@ public:
 	std::vector<std::vector<double> > get_hares();//returns a vector of vectors with size grid_size_x * grid_size_y with the density of hares
 	std::vector<std::vector<double> > get_pumas();
 
+	//helper functions for constructor
+	int readinfile(const std::string, std::vector<std::vector<double> >&);
+	void generateRandomDensity(std::vector<std::vector<double> > &, std::vector<std::vector<double> > const &);
+
 
 };
+
+struct Params{double r,a,b,m,k,l,dt;};
+
 
 #endif
--- src/landscape.cpp
+++ src/landscape.cpp
@@ -1,54 +1,61 @@
 #include "../include/landscape.h"
 
 
-landscape::landscape()
+
+landscape::landscape(const Params &pars, const std::string mapPath, const std::string pumaspath = "", const std::string harespath = "")
 {
-	grid_size_x = 9;
-	grid_size_y=9;
+	//Still To-Do:
+	// Add try, catch blocks for file reading
+	// Tidy code with c in mind - i.e. maybe use more pass by reference, add const etc.
 
-	resizeVec(grid,grid_size_x,grid_size_y);	// resize and initialize grid with some default values for now	
-	for(int i=0;i!=grid_size_x;i++)
-		for(int j=0;j!=grid_size_y;j++)
-			if( i==0 || i==grid_size_x-1 || j==0 || j==grid_size_y-1)
-				grid[i][j]=0;
-			else if( (i==4 && j==4) || (i==3 && j==4))
-				grid[i][j]=0;
-			else
-				grid[i][j]=1;
 
-	resizeVec (N,grid_size_x,grid_size_y); // N counts the number of neighbors for each cell in order to be used from the progress function
-	for(int i=1;i!=grid_size_x-1;i++)
-		for(int j=1;j!=grid_size_y-1;j++)
-			N[i][j]=grid[i+1][j]+grid[i][j+1]+grid[i-1][j]+grid[i][j-1];
-
-	resizeVec(pumas,grid_size_x,grid_size_y);
-	resizeVec(hares,grid_size_x,grid_size_y);
-
-	resizeVec(pumas_old,grid_size_x,grid_size_y);
-	resizeVec(hares_old,grid_size_x,grid_size_y);
-
+	//Extract size of the landscape
+    std::ifstream mapfile;
+    mapfile.open(mapPath.c_str());
+    if (!mapfile.is_open())
+    {
+        std::cout<< "Failed to open file: " <<  mapPath << std::endl;
+    }
+	else{
+    mapfile >> grid_size_x >> grid_size_y;
+	//Add space for a ring of water. Note vector initializes to 0 so the ring will be added automatically
+	grid_size_x += 2; grid_size_y += 2;
+	map.resize(grid_size_x, std::vector<double>(grid_size_y,0)); // Initialize the map matrix
+	hares.resize(grid_size_x, std::vector<double>(grid_size_y,0)); // Initialize the hare density matrix
+	pumas.resize(grid_size_x, std::vector<double>(grid_size_y,0)); // Initialize the puma density matrix
+	mapfile.close();
+	}
 	
+	if(!readinfile(mapPath, map))
+		std::cout<< "Failed to open file: " <<  mapPath << std::endl;
+		
+	
+	
+	std::srand(std::time(NULL)); //Seed random number generator
+	
+	if(pumaspath != "")
+		readinfile(pumaspath, pumas);
+	else
+		generateRandomDensity(pumas,map);
+	if(harespath != "")
+		readinfile(harespath, hares);
+	else
+		generateRandomDensity(hares,map);
+						
+	
+	//Console output - To see the map
+	
+	// for(int x=0; x<grid_size_x; x++){
+		// for(int y=0; y<grid_size_y; y++){			
+			// std::cout << map[x][y];
+		// }
+		// std::cout << std::endl;
+	// }	
+	// r = pars.r; a = pars.a; b = pars.b; m = pars.m; k = pars.k; l = pars.l; 
 
-	for(int i=0;i!=grid_size_x;i++)
-		for(int j=0;j!=grid_size_y;j++)
-		{
-			if( grid[i][j]==1)
-			{
-				if( i==0 || i==grid_size_x-1 || j==0 || j==grid_size_y-1)
-				{
-					pumas[i][j]=0;
-					hares[i][j]=0;
-					continue;
-				}
-				hares[i][j]= i*1./2;
-				pumas[i][j]= j*1./2;
-			}
-		}
-			
-			
-	r= 0.08;a=0.04;b=0.02;m=0.06;k=0.2;l=0.2;dt=0.4; // where ...
-	//onstractor will set the size of pumas & hares
-	// fill them (if this will be a seperate class)
+	// std::cout << "r=" << r << " a=" << a << " b=" << b<< " m=" << m<< " k=" << k<< " l=" << l << std::endl;
+	
+	// std::cout << "size of map: " << map.size() << " by " << map[0].size() << std::endl;
 }
 
 void landscape::progress()
@@ -154,7 +161,6 @@ for (int i = 0; i < grid_size_x; i++ ) {
    }
 }
 
-
 std::vector<std::vector<double> > landscape::get_hares()
 {
 return hares;
@@ -164,4 +170,42 @@ return hares;
 std::vector<std::vector<double> > landscape::get_pumas()
 {
 return pumas;
+}
+
+//helper functions for constructor
+
+int landscape::readinfile(const std::string path, std::vector<std::vector<double> >& matrix)
+{
+	int xs = matrix.size(); int ys = matrix[0].size();
+	std::ifstream file;
+    file.open(path.c_str());
+    if (!file.is_open())
+    {
+        std::cout<< "Failed to open file " << path << std::endl;
+		return 0;
+    }
+	else{	
+	int tmp;
+	file >> tmp >> tmp; // Ignore size values
+	
+	for(int x=1; x<(xs-1); x++){        // Ingnore the first and last entries as they are the water ring
+		for(int y=1; y<(ys-1); y++){
+			file >> tmp;
+			matrix[x][y] = tmp;
+		}
+	}
+	file.close();
+	return 1;
+    }
+}
+
+void landscape::generateRandomDensity(std::vector<std::vector<double> > &matrix, std::vector<std::vector<double> > const &map)
+{
+	int xs = matrix.size(); int ys = matrix[0].size();
+	for(int x=0; x<xs; x++){        
+		for(int y=0; y<ys; y++){
+			if(map[x][y]==1)
+			    matrix[x][y] = std::rand()%6;  //assign random density between 0 and 5
+		}
+	}
 }
