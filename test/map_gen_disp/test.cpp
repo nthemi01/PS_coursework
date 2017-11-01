934dff2ea6e5ef3491b6049ce3ea61c9de4b80ac
mgxnzhu <mgxzhu@gmail.com>
2017-11-01 01:58:35 +0000

    test CIMG

--- test/map_gen_disp/test.cpp
+++ test/map_gen_disp/test.cpp
@@ -11,6 +11,8 @@ using namespace cimg_library;
 
 int main(){
 
+/*
+// colormap for testing
   vector<vector<double>> colormap;
 
   for (int i = 0; i < 50; ++i) {
@@ -22,6 +24,7 @@ int main(){
   }
 
   output::savefig(colormap, "colorbar.ppm", true);
+*/
 
   auto map = setmap(40,40,1.7,10,4);
   vector<vector<bool>> mask;
@@ -37,13 +40,13 @@ int main(){
   auto density = setmap(40,40,1.7,10,4);
 
   output::savefig(density, mask, "test.ppm", true);
-/*
-  CImg<unsigned char> img(800,640,1,3); // 640x800 image, 8bit color
-  img.fill(0); // fill image with black (0)
-  const unsigned char purple[] = {255, 0, 255}; // define purple
+
+  CImg<unsigned int> img(400,400,1,3); // 640x800 image, 8bit color
+
+  const unsigned int purple[] = {255, 0, 255}; // define purple
   img.draw_text(100,100,"hello world",purple);
   img.display("Hello World display");
-*/
+
 
   return 0;
 }
