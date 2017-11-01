3ab4ca2abfed52ebcd281a8868f0e592510c2cdb
mgxnzhu <31207233+mgxnzhu@users.noreply.github.com>
2017-11-01 02:55:08 +0000

    Update landscape.h

--- include/landscape.h
+++ include/landscape.h
@@ -55,9 +55,8 @@ public:
 	double average_hares(); //return the average value of hares along the "dry" squares
 	double average_pumas();
 
-	void printhares(); //prints to the screen the density matrix of hares
-	void printpumas();
-	void printPPM();
+	void save_densities(std::string filename_puma, std::string filename_hare, bool color_toggle); //prints to the screen the density matrix
+	void print_densities();
 
 	std::vector<std::vector<double> > get_hares();//returns a vector of vectors with size grid_size_x * grid_size_y with the density of hares
 	std::vector<std::vector<double> > get_pumas();
