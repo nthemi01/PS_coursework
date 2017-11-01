964e2641e0cc6e826bd2dc707248ab5aa95fa2ff
mgxnzhu <mgxzhu@gmail.com>
2017-11-01 22:29:13 +0000

    add map_gen_test

---
+++ test/map_gen_disp/map_gen_example.cpp
@@ -0,0 +1,37 @@
+#include "noise.h"
+#include "randommap.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "disdensity.h"
+#include "CImg.h"
+
+using std::vector;
+using namespace cimg_library;
+
+int main(){
+
+  // test saving a masked map
+  auto map = map_gen(40,40,1.7,10,4);
+  vector<vector<bool>> mask;
+
+  for (auto& line : map) {
+    vector<bool> maskline;
+    for (auto& pixel : line) {
+      maskline.push_back(pixel>0.2);
+    }
+    mask.push_back(maskline);
+  }
+
+  auto density = map_gen(40,40,1.7,10,4);
+  auto dump = output::display(density, true, "test1.ppm");
+  dump = output::display(density, mask, true, "test2.ppm");
+
+  std::ofstream fout("test.dat");
+  fout << "40 40\n" << mask;
+  fout << std::flush;
+  fout.close();
+
+  return 0;
+}
+
