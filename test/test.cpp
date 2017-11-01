c68d4c5d8cf9804c045edc2937dfea02244b7430
foundnet <foundnet@localhost.localdomain>
2017-11-01 06:30:11 +0000

    Add gtest framework

---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "landscape.h"
+
+TEST(LandTestSuit1, TestCase1)
+{
+    EXPECT_EQ(-1,-1);
+}
+
+TEST(LandTestSuit2, TestCase2)
+{
+    ASSERT_EQ(8,0);
+}
+
+int main(int argc,char **argv)
+{
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
