b1790ccbdd28c3503791c3a9db87bb2979710c11
nthemi01 <themistokleous.neofytos@gmail.com>
2017-11-03 00:38:51 +0000

    finished my testing

--- test/compile_Neofytos_test.sh
+++ test/compile_Neofytos_test.sh
@@ -1,4 +1,4 @@
 g++ -c ../src/landscape.cpp -std=c++11
 g++ -c test_Neofytos.cpp -lpthread -std=c++11
-g++ test_Neofytos.o landscape.o -o test -lgtest -lpthread -std=c++11
-./test 
+g++ test_Neofytos.o landscape.o -o test_Neofytos -lgtest -lpthread -std=c++11
+./test_Neofytos 
--- test/test_Neofytos.cpp
+++ test/test_Neofytos.cpp
@@ -44,13 +44,48 @@ TEST(landscape, initial_densities)
 	for(int i=0;i<7;i++)
 		for(int j=0;j<7;j++)
 		{
-		 	ASSERT_DOUBLE_EQ(real_pumas[i][j],test_pumas[i][j]);
-			ASSERT_DOUBLE_EQ(real_hares[i][j],test_hares[i][j]);
+		 	ASSERT_DOUBLE_EQ(test_pumas[i][j],real_pumas[i][j]);
+			ASSERT_DOUBLE_EQ(test_hares[i][j],real_hares[i][j]);
 		}
 }
 
 
-TEST(landscape, progress)
+TEST(landscape,neighbors)
+{
+	const std::string kMapPath = "test_map1.dat";
+	const std::string kHaresPath = "test_hares1.dat";
+	const std::string kPumasPath = "test_pumas1.dat";
+	Params pars;
+	pars.r = 0.8;
+	pars.a = 0.04;
+	pars.b = 0.02;
+	pars.m = 0.06;
+	pars.k = 0.2;
+	pars.l = 0.2;
+	pars.dt = 0.4;
+	landscape land(pars,kMapPath,kPumasPath,kHaresPath);
+
+	std::vector<std::vector<int> > test_neighbors = land.get_neighbors();
+
+
+	//Values on the array were evaluated by hand
+	std::vector<std::vector<int> > real_neighbors    {{0, 0, 0, 0, 0, 0, 0}, 
+							{0, 2, 3, 3, 3, 2, 0}, 
+							{0, 3, 4, 3, 3, 3, 0}, 
+							{0, 3, 3, 0, 0, 2, 0}, 
+							{0, 3, 3, 0, 0, 2, 0}, 
+							{0, 2, 2, 0, 0, 1, 0}, 
+							{0, 0, 0, 0, 0, 0, 0}}; 
+
+	
+	for(int i=0;i<7;i++)
+		for(int j=0;j<7;j++)
+		{
+			ASSERT_EQ(real_neighbors[i][j],test_neighbors[i][j]);
+		}
+}
+
+TEST(landscape, single_progress)
 {
 	const std::string kMapPath = "test_map1.dat";
 	const std::string kHaresPath = "test_hares1.dat";
@@ -98,14 +133,13 @@ TEST(landscape, progress)
 	                 for(int i=0;i<7;i++)
 		for(int j=0;j<7;j++)
 		{
-		 	//ASSERT_DOUBLE_EQ(real_pumas[i][j],test_pumas[i][j]);
-			ASSERT_DOUBLE_EQ(real_hares[i][j],test_hares[i][j]);
+		 	ASSERT_DOUBLE_EQ(test_pumas[i][j],real_pumas[i][j]);
+			ASSERT_DOUBLE_EQ(test_hares[i][j],real_hares[i][j]);
 		}
 }
 
 
-
-TEST(landscape,neighbors)
+TEST(landscape, 10_progress)
 {
 	const std::string kMapPath = "test_map1.dat";
 	const std::string kHaresPath = "test_hares1.dat";
@@ -120,25 +154,46 @@ TEST(landscape,neighbors)
 	pars.dt = 0.4;
 	landscape land(pars,kMapPath,kPumasPath,kHaresPath);
 
-	std::vector<std::vector<int> > test_neighbors = land.get_neighbors();
+	for(int k=0;k<10;k++)
+		land.progress();
+
+	std::vector<std::vector<double> > test_hares = land.get_hares();
+	std::vector<std::vector<double> > test_pumas = land.get_pumas();
 
 
-	//Values on the array were evaluated by hand
-	std::vector<std::vector<int> > real_neighbors    {{0, 0, 0, 0, 0, 0, 0}, 
-							{0, 2, 3, 3, 3, 2, 0}, 
-							{0, 3, 4, 3, 3, 3, 0}, 
-							{0, 3, 3, 0, 0, 2, 0}, 
-							{0, 3, 3, 0, 0, 2, 0}, 
-							{0, 2, 2, 0, 0, 1, 0}, 
+	//Values on the arrays were calculated using a different method (wolfram Mathematica)
+	std::vector<std::vector<double> > real_pumas {{0, 0, 0, 0, 0, 0, 0}, 
+							{0, 3.36905, 4.19512, 5.3256, 5.76663,5.13713, 0},
+							 {0, 3.46986, 3.98811, 4.88618, 5.12091, 4.41509,0},
+							 {0, 3.06879, 3.29001, 0, 0, 3.65654, 0}, 
+							{0, 2.60071, 2.78122, 0, 0, 2.5506, 0}, 
+							{0, 2.03098, 2.19, 0, 0, 2.15166, 0}, 
 							{0, 0, 0, 0, 0, 0, 0}}; 
+
+
+
+       	std::vector<std::vector<double> > real_hares {{0, 0, 0, 0, 0, 0, 0},
+							 {0, 25.2136, 26.0136, 26.6728, 26.5413, 24.5331, 0},
+							 {0, 28.5986, 28.1866, 27.4487, 27.1147, 26.4163, 0},
+							 {0, 25.805, 27.1496, 0, 0, 29.9153, 0}, 
+							{0, 24.3826, 27.1328, 0,  0, 29.6774, 0}, 
+							{0, 22.4783, 25.8482, 0, 0, 33.8322, 0}, 
+							{0, 0, 0, 0, 0, 0, 0}}; 
+
+
+
 
 	
 	for(int i=0;i<7;i++)
 		for(int j=0;j<7;j++)
 		{
-			ASSERT_EQ(real_neighbors[i][j],test_neighbors[i][j]);
+			//Double numbers after 10 iterations diverge slightly between c++ code and wolfram Mathemtica,
+			//so we use delta = 1e-4 tou account for that
+			ASSERT_NEAR (test_pumas[i][j],real_pumas[i][j],0.0001);
+			ASSERT_NEAR (test_hares[i][j],real_hares[i][j],0.0001);
 		}
 }
+
 
 
 
