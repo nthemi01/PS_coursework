75fc79c1b04bbeb41cbb2d4f38e4c81d6859be8b
mgxnzhu <mgxzhu@gmail.com>
2017-11-03 23:29:20 +0000

    google test

--- test/map_gen_disp/test.sh
+++ test/map_gen_disp/test.sh
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 g++ -c  ../../src/mapgenrator/randommap.cpp ../../src/mapgenrator/noise.cpp -std=c++11
 g++ -c map_gen_test.cpp -lpthread -std=c++11
 g++ map_gen_test.o noise.o randommap.o -o map_gtest -lgtest -lpthread -std=c++11
+=======
+g++ -c ../../src/landscape.cpp ../../src/mapgenrator/randommap.cpp ../../src/mapgenrator/noise.cpp -std=c++11 -I../../include
+g++ -c map_gen_test.cpp -lpthread -std=c++11 -I../../include
+g++ map_gen_test.o landscape.o noise.o randommap.o -o map_gtest -lgtest -lpthread -std=c++11
+>>>>>>> 4d4ec1fee899a2eaac86846bc9cde71b9f5fe0e1
 ./map_gtest
