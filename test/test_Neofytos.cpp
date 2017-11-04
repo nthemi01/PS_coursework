8a1274ab9b0568caac24e4f5177883bda3035757
nthemi01 <themistokleous.neofytos@gmail.com>
2017-11-04 01:12:00 +0000

    Changed the code due to transposed matrix

--- test/test_Neofytos.cpp
+++ test/test_Neofytos.cpp
@@ -25,21 +25,25 @@ TEST(landscape, initial_densities)
 
 
 	//Values on the two arrays are the same as the input file	
-	std::vector<std::vector<double> > real_pumas {{0,0,0,0,0,0,0},
-					   {0,2,3,4,5,4,0},
-                                           {0,1,2,3,3,1,0}, 
-					   {0,3,2,0,0,3,0},
-					   {0,2,2,0,0,1,0},
-					   {0,1,1,0,0,1,0},	
-					   {0,0,0,0,0,0,0}};
+	std::vector<std::vector<double> > real_pumas {{0, 0, 0, 0, 0, 0, 0},
+							 {0, 2, 1, 3, 2, 1, 0}, 
+							{0, 3, 2, 2, 2, 1,  0}, 
+							{0, 4, 3, 0, 0, 0, 0},
+							 {0, 5, 3, 0, 0, 0, 0},
+							 {0, 4, 1, 3, 1, 1, 0},
+							 {0, 0, 0, 0, 0, 0, 0}}; 
 
-	std::vector<std::vector<double> > real_hares {{0,0,0,0,0,0,0},
-					   {0,1,2,3,4,2,0},
-                                           {0,4,3,2,3,1,0}, 
-					   {0,1,2,0,0,4,0},
-					   {0,2,3,0,0,1,0},
-					   {0,1,2,0,0,3,0},	
-					   {0,0,0,0,0,0,0}};
+std::vector<std::vector<double> > real_hares {{0, 0, 0, 0, 0, 0, 0},
+						 {0, 1, 4, 1, 2, 1, 0},
+						 {0, 2, 3, 2, 3, 2, 0}, 
+						{0, 3, 2, 0, 0, 0, 0},
+						 {0, 4, 3, 0, 0, 0, 0},
+						 {0, 2, 1, 4, 1, 3, 0},
+						 {0, 0, 0, 0, 0, 0, 0}};
+
+
+
+
 
 	for(int i=0;i<7;i++)
 		for(int j=0;j<7;j++)
@@ -49,41 +53,8 @@ TEST(landscape, initial_densities)
 		}
 }
 
-//Check that the array that counts "dry" neighbors is computed correctly
-TEST(landscape,neighbors)
-{
-	const std::string kMapPath = "test_map1.dat";
-	const std::string kHaresPath = "test_hares1.dat";
-	const std::string kPumasPath = "test_pumas1.dat";
-	Params pars;
-	pars.r = 0.8;
-	pars.a = 0.04;
-	pars.b = 0.02;
-	pars.m = 0.06;
-	pars.k = 0.2;
-	pars.l = 0.2;
-	pars.dt = 0.4;
-	landscape land(pars,kMapPath,kPumasPath,kHaresPath);
-
-	std::vector<std::vector<int> > test_neighbors = land.get_neighbors();
 
 
-	//Values on the array were evaluated by hand
-	std::vector<std::vector<int> > real_neighbors    {{0, 0, 0, 0, 0, 0, 0}, 
-							{0, 2, 3, 3, 3, 2, 0}, 
-							{0, 3, 4, 3, 3, 3, 0}, 
-							{0, 3, 3, 0, 0, 2, 0}, 
-							{0, 3, 3, 0, 0, 2, 0}, 
-							{0, 2, 2, 0, 0, 1, 0}, 
-							{0, 0, 0, 0, 0, 0, 0}}; 
-
-	
-	for(int i=0;i<7;i++)
-		for(int j=0;j<7;j++)
-		{
-			ASSERT_EQ(real_neighbors[i][j],test_neighbors[i][j]);
-		}
-}
 
 //Check that hares and pumas have the correct values after 1 iterration
 TEST(landscape, single_progress)
@@ -108,24 +79,24 @@ TEST(landscape, single_progress)
 
 	//Values on the arrays were calculated using a different method (wolfram Mathematica)
 	std::vector<std::vector<double> > real_pumas  {{0, 0, 0, 0, 0, 0, 0}, 
-							{0, 1.968, 2.896, 3.92, 4.72, 3.808, 0}, 
-							{0, 1.328, 2.08, 2.976, 3., 1.544, 0}, 
-							{0, 2.632, 2.064, 0, 0, 2.704, 0},
-							{0, 1.984, 1.92, 0, 0, 1.144, 0}, 
-							{0, 1.064, 1.072, 0, 0, 1., 0}, 
-							{0, 0,     0,     0, 0, 0, 0}};
+							{0, 1.968, 1.328, 2.632, 1.984, 1.064, 0},
+							 {0, 2.896, 2.08, 2.064, 1.92, 1.072, 0}, 
+							{0, 3.92, 2.976, 0, 0, 0, 0}, 
+							{0, 4.72, 3., 0, 0, 0, 0},
+							 {0, 3.808, 1.544, 2.704, 1.144, 1., 0},
+							 {0, 0, 0, 0, 0, 0, 0}};
 
 
 
 
 
-       	std::vector<std::vector<double> > real_hares   {{0, 0, 0, 0, 0, 0, 0}, 
-							{0, 1.608, 2.624, 3.688, 4.64, 2.592, 0},
-							{0,4.656, 3.704, 2.784, 3.656, 1.784, 0}, 
-							{0, 1.672, 2.656, 0, 0, 4.608, 0}, 
-							{0, 2.496, 3.624, 0, 0, 1.704, 0}, 
-							{0, 1.464, 2.608, 0,0, 3.752, 0}, 
-							{0, 0, 0, 0, 0, 0, 0}};
+       	std::vector<std::vector<double> > real_hares {{0, 0, 0, 0, 0, 0, 0},
+							 {0, 1.608, 4.656, 1.672, 2.496, 1.464, 0},
+							 {0, 2.624, 3.704, 2.656, 3.624, 2.608, 0},
+							 {0, 3.688, 2.784, 0, 0, 0, 0}, 
+							{0, 4.64, 3.656, 0, 0, 0, 0},
+							 {0, 2.592, 1.784, 4.608, 1.704,  3.752, 0},
+							 {0, 0, 0, 0, 0, 0, 0}};
 
 
 
@@ -164,23 +135,23 @@ TEST(landscape, 10_progress)
 
 
 	//Values on the arrays were calculated using a different method (wolfram Mathematica)
-	std::vector<std::vector<double> > real_pumas {{0, 0, 0, 0, 0, 0, 0}, 
-							{0, 3.36905, 4.19512, 5.3256, 5.76663,5.13713, 0},
-							 {0, 3.46986, 3.98811, 4.88618, 5.12091, 4.41509,0},
-							 {0, 3.06879, 3.29001, 0, 0, 3.65654, 0}, 
-							{0, 2.60071, 2.78122, 0, 0, 2.5506, 0}, 
-							{0, 2.03098, 2.19, 0, 0, 2.15166, 0}, 
+	std::vector<std::vector<double> > real_pumas  {{0, 0, 0, 0, 0, 0, 0}, 
+							{0, 3.36905, 3.46986, 3.06879, 2.60071, 2.03098, 0}, 
+							{0, 4.19512, 3.98811, 3.29001, 2.78122, 2.19, 0},
+							 {0, 5.3256, 4.88618, 0, 0, 0, 0}, 
+							{0, 5.76663, 5.12091, 0, 0, 0, 0},
+							 {0, 5.13713, 4.41509, 3.65654, 2.5506, 2.15166, 0}, 
 							{0, 0, 0, 0, 0, 0, 0}}; 
 
 
 
-       	std::vector<std::vector<double> > real_hares {{0, 0, 0, 0, 0, 0, 0},
-							 {0, 25.2136, 26.0136, 26.6728, 26.5413, 24.5331, 0},
-							 {0, 28.5986, 28.1866, 27.4487, 27.1147, 26.4163, 0},
-							 {0, 25.805, 27.1496, 0, 0, 29.9153, 0}, 
-							{0, 24.3826, 27.1328, 0,  0, 29.6774, 0}, 
-							{0, 22.4783, 25.8482, 0, 0, 33.8322, 0}, 
-							{0, 0, 0, 0, 0, 0, 0}}; 
+       	std::vector<std::vector<double> > real_hares  {{0, 0, 0, 0, 0, 0, 0},
+ 							{0, 25.2136, 28.5986, 25.805, 24.3826, 22.4783, 0},
+							 {0, 26.0136, 28.1866, 27.1496, 27.1328, 25.8482, 0},
+							 {0, 26.6728, 27.4487, 0, 0, 0, 0},
+							 {0, 26.5413, 27.1147, 0, 0, 0, 0},
+							 {0, 24.5331, 26.4163, 29.9153, 29.6774, 33.8322, 0},
+							 {0, 0, 0, 0, 0, 0, 0}}; 
 
 	
 	for(int i=0;i<7;i++)
